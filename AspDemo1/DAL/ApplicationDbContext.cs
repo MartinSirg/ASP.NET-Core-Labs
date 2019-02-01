19b9cbaeb0cc1716ed24ef224805090befeb9c49
masirg <masirg@ttu.ee>
2019-02-01 15:43:40 +0200

    Finished lab 1

---
+++ AspDemo1/DAL/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace DAL
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<ContactType> ContactTypes { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+            
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var converter = new BoolToZeroOneConverter<Int16>();
+
+            modelBuilder
+                .Entity<IdentityUser>()
+                .Property(user => user.LockoutEnabled)
+                .HasConversion(converter);
+            
+            modelBuilder
+                .Entity<IdentityUser>()
+                .Property(user => user.TwoFactorEnabled)
+                .HasConversion(converter);
+
+            modelBuilder
+                .Entity<IdentityUser>()
+                .Property(user => user.EmailConfirmed)
+                .HasConversion(converter);
+            
+            modelBuilder
+                .Entity<IdentityUser>()
+                .Property(user => user.PhoneNumberConfirmed)
+                .HasConversion(converter);
+            
+            base.OnModelCreating(modelBuilder);
+                  
+        }
+
+    }
+}
---
+++ AspDemo1/Domain/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Person : BaseEntity
+    {
+        [MaxLength(64)]
+        [MinLength(1)]
+        [Required]
+        public string FirstName { get; set; }
+        
+        [MaxLength(64)]
+        [MinLength(1)]
+        [Required]
+        public string  LastName { get; set; }
+
+        public List<Contact> Contacts { get; set; }
+
+        public string FirstLastName => $"{FirstName} {LastName}";
+        public string LastFirstName => $"{LastName} {FirstName}";
+    }
+}

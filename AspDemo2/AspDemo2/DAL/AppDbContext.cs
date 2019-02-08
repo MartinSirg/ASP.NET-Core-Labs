4a66c0fa091fd51e532948fde3920c82ca328539
masirg <masirg@ttu.ee>
2019-02-08 14:07:41 +0200

    base

---
+++ AspDemo2/AspDemo2/DAL/AppDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace DAL
+{
+    public class AppDbContext: IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var converter = new BoolToZeroOneConverter<Int16>();
+
+            modelBuilder
+                .Entity<IdentityUser>()
+                .Property(user => user.LockoutEnabled)
+                .HasConversion(converter);
+            
+            modelBuilder
+                .Entity<IdentityUser>()
+                .Property(user => user.TwoFactorEnabled)
+                .HasConversion(converter);
+
+            modelBuilder
+                .Entity<IdentityUser>()
+                .Property(user => user.EmailConfirmed)
+                .HasConversion(converter);
+            
+            modelBuilder
+                .Entity<IdentityUser>()
+                .Property(user => user.PhoneNumberConfirmed)
+                .HasConversion(converter);
+            
+            base.OnModelCreating(modelBuilder);
+                  
+        }
+    }
+}
